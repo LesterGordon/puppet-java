bb50ab6ea7a87749339ba820614d73a6ebd8da3e
Lester Gordon <gretsel@gmail.com>
2014-04-29 13:33:04 -0500

    Add version parameter.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,24 +4,25 @@
 #
 #    include java
 class java (
+  $version = '7',
   $update_version = '55',
   $base_download_url = 'https://s3.amazonaws.com/boxen-downloads/java'
 ) {
   include boxen::config
 
-  $jre_url = "${base_download_url}/jre-7u${update_version}-macosx-x64.dmg"
-  $jdk_url = "${base_download_url}/jdk-7u${update_version}-macosx-x64.dmg"
+  $jre_url = "${base_download_url}/jre-${version}u${update_version}-macosx-x64.dmg"
+  $jdk_url = "${base_download_url}/jdk-${version}u${update_version}-macosx-x64.dmg"
   $wrapper = "${boxen::config::bindir}/java"
-  $jdk_dir = "/Library/Java/JavaVirtualMachines/jdk1.7.0_${update_version}.jdk"
+  $jdk_dir = "/Library/Java/JavaVirtualMachines/jdk1.${version}.0_${update_version}.jdk"
   $sec_dir = "${jdk_dir}/Contents/Home/jre/lib/security"
 
   package {
-    "jre-7u${update_version}.dmg":
+    "jre-${version}u${update_version}.dmg":
       ensure   => present,
       alias    => 'java-jre',
       provider => pkgdmg,
       source   => $jre_url ;
-    "jdk-7u${update_version}.dmg":
+    "jdk-${version}u${update_version}.dmg":
       ensure   => present,
       alias    => 'java',
       provider => pkgdmg,
